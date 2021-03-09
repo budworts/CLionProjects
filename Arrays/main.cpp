eee6a9b7d481b98167aa7c5494de009714f4d6de
budworts <budworts@googlemail.com>
2021-03-09 09:37:14 +0000

    Arrays 09/03/2021

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -4,5 +4,14 @@ int main() {
 
     std::cout << "c++ version: " << __cplusplus << std::endl;
 
+    int arr[5];
+
+    for (auto i=0;i<5;i++) {
+        std::cout << arr[i] << std::endl;
+    }
+
+
+
+
     return 0;
 }
