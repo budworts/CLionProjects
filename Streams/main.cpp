430dd69114393b8cfb72b202cf0d5ad594d1000a
Steven Budworth <budworts@googlemail.com>
2021-03-13 14:00:40 +0000

    20210313 14:00

--- Streams/main.cpp
+++ Streams/main.cpp
@@ -73,24 +73,30 @@ int main() {
         std::cout << num3 << std::endl;
         std::cout << std::endl;
 
+        std::cout << "setprecision\n";
         std::cout << std::setprecision(9);
         std::cout << num1 << std::endl;
         std::cout << num2 << std::endl;
         std::cout << num3 << std::endl;
         std::cout << std::endl;
 
+        std::cout << "fixed\n";
         std::cout << std::fixed;
         std::cout << num1 << std::endl;
         std::cout << num2 << std::endl;
         std::cout << num3 << std::endl;
         std::cout << std::endl;
 
-        std::cout << std::setprecision(3) << std::scientific << std::uppercase << std::showpos;
+        std::cout << "setprecision etc...\n";
+        std::cout << std::setprecision(3) << std::scientific
+            << std::uppercase << std::showpos;
         std::cout << num1 << std::endl;
         std::cout << num2 << std::endl;
         std::cout << num3 << std::endl;
         std::cout << std::endl;
 
+
+        std::cout << std::setprecision(-1);
 //        std::cout.unsetf(std::ios::scientific | std::ios::fixed );
         std::cout << std::resetiosflags(std::ios::floatfield);
 //        std::cout << std::resetiosflags(std::ios::fixed);
@@ -103,6 +109,27 @@ int main() {
         std::cout << num3 << std::endl;
     }
 
+    {
+        std::cout << "\n++++ Align & Fill ++++\n";
+
+        double num{1234.5678};
+        std::string hello{"Hello"};
+
+        std::cout << "12345678901234567890\n";
+        std::cout << "--------------------\n";
+
+        std::cout << std::setw(10) << num
+            << std::setw(10) << hello << "\n\n";
+
+        std::cout << std::setfill('-');
+
+        std::cout << std::setw(10) << std::left << num
+            << std::setw(15)  << std::right << hello << std::endl // right is the default
+            << std::setw(7) << hello << std::endl;
+
+
+
+    }
 
     return 0;
 }
