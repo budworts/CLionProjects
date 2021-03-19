992819d763275a53b66094b52ae54d7e424cb107
Steven Budworth <budworts@googlemail.com>
2021-03-19 14:01:32 +0000

    20210319 - 1401

--- SystemProgramming/Lambda/main.cpp
+++ SystemProgramming/Lambda/main.cpp
@@ -9,8 +9,29 @@ int main() {
     vector<int> v{1, 2, 3, 4, 5, 6};
     string prefix{"0"};
 
-    for_each(begin(v), end(v), [](int x) { cout << x << endl; });
-    for_each(begin(v), end(v), [&prefix](int x) { cout << prefix << x << endl; });
+    for_each(begin(v), end(v), [](int x) {
+                 cout << x << endl;
+             }
+    );
+
+    for_each(begin(v), end(v), [&prefix](int x) {
+                 cout << prefix << x << endl;
+             }
+    );
+
+//    vector<int> r;
+//    for_each(begin(v), end(v), [](int x) -> vector<int> {
+//                 if (x % 2) {
+//                     return {1, 2};
+//                 } else {
+//                     return {3, 4};
+//                 }
+//             }
+//    );
+//
+//    for ( auto i: r) {
+//        cout << i << endl;
+//    }
 
     return 0;
 }
