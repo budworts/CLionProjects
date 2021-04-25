896399b147a8b0b78bf4cdb3ee499cde2195a44d
budworts <budworts@googlemail.com>
2021-04-25 14:11:38 +0100

    Added 25042021

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -4,11 +4,69 @@ int main() {
 
     std::cout << "c++ version: " << __cplusplus << std::endl;
 
-    int arr[5];
+    int ai1[10] = {5,10,15,20,25};  // Array 5,10,15,20,25,0,0,0,0,0
+
+    for (auto i=0;i<10;i++) {
+        std::cout << ai1[i] << " ";
+    }
+
+    std::cout << std::endl;
+
+    int ai2[] = {5,10,15,20,25};
 
     for (auto i=0;i<5;i++) {
-        std::cout << arr[i] << std::endl;
+        std::cout << ai2[i] << " ";
     }
+
+    std::cout << std::endl;
+
+    int ai3[50];  // Junk
+
+    for (auto i=0;i<50;i++) {
+        std::cout << ai3[i] << " ";
+    }
+
+    std::cout << std::endl;
+
+    int ai4[50] = {0};
+
+    for (auto i=0;i<50;i++) {
+        std::cout << ai4[i] << " ";
+    }
+
+    std::cout << std::endl;
+
+    // ***********************
+    int ai5[] = {0,1,2,3,4,5,6};
+
+    for (auto i:ai5) {
+        std::cout << i << " ";
+    }
+
+    std::cout << std::endl;
+
+    int ai6[10] = {3,4,5,6};
+
+    for (auto i:ai6) {
+        std::cout << i << " ";
+    }
+
+    std::cout << std::endl;
+    // ***********************
+
+    char ac1[] = {'A', 66, 'C', 68};
+
+    for (auto i:ac1) {
+        std::cout << i << " ";
+    }
+
+    std::cout << std::endl;
+
+    for (int i:ac1) {
+        std::cout << i << " ";
+    }
+
+    std::cout << std::endl;
 
     return 0;
 }
